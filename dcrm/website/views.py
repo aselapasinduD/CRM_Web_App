59c4a8b0895629023ff5ff8ab7a8d78ccbc85271
Asela Pasindu <asela.pasindu.dias17@gmail.com>
2024-02-17 05:49:21 +0530

    Setup register_user in to website and create records in database

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
+from .models import Record
 
 def home(request):
     # Check to see if logging in
@@ -16,10 +18,26 @@ def home(request):
         else:
             messages.success(request, "There Was an Error Loggin in, Please Try Again.")
             return redirect('home')
-        
-    return render(request, 'home.html', {})
+    records = Record.objects.all()
+    return render(request, 'home.html', {'records': records})
 
 def logout_user(request):
     logout(request)
     messages.success(request, "You Have Been Logged Out...")
     return redirect('home')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You have been successfully registered! Welcome!")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'register.html', {'form': form})
