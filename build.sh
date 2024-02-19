b4c489e67133029263f8563d8afc83516bbd5cb2
Asela Pasindu <asela.pasindu.dias17@gmail.com>
2024-02-19 05:51:11 +0530

    Modify build.sh file

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -o errexit
 pip install -r requirements.txt
 
 # Convert static asset files
-python ./dcrm/manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python ./dcrm/manage.py migrate
+python manage.py migrate
